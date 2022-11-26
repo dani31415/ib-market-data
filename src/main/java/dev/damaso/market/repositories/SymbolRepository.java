2c6ad9ad5033799954af96d10efa635106131085
dani31415@ <dani31415@gmail.com>
2022-11-26 20:29:53 +0100

    Fixed SQL statement.

--- src/main/java/dev/damaso/market/repositories/SymbolRepository.java
+++ src/main/java/dev/damaso/market/repositories/SymbolRepository.java
@@ -9,7 +9,7 @@ import dev.damaso.market.entities.Symbol;
 public interface SymbolRepository extends CrudRepository<Symbol, Integer> {
     @Cacheable("symbols")
 
-    @Query("SELECT s FROM Symbol s WHERE s.shortName = ?1 AND NOT disabled")
+    @Query("SELECT s FROM Symbol s WHERE s.shortName = ?1 AND s.disabled = false")
     Symbol findSymbolByShortName(String shortName);
 
     @Query(nativeQuery = true, value = "SELECT s.* FROM symbol s WHERE s.ib_conid IS NOT NULL ORDER BY s.id ASC")
