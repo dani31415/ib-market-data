23e789b1687694e16b89bed8e7b27046ae4fb9fe
dani31415@ <dani31415@gmail.com>
2022-12-08 18:50:23 +0100

    Added fitler by volume in mean.

--- src/main/java/dev/damaso/market/repositories/PeriodRepository.java
+++ src/main/java/dev/damaso/market/repositories/PeriodRepository.java
@@ -14,12 +14,16 @@ public interface PeriodRepository extends CrudRepository<Period, Integer> {
 
     List<Period> findByUpdated(boolean updated);
 
-    @Query("SELECT avg(i2.open/i1.open) AS mean FROM Item AS i1 " + 
-	       "INNER JOIN Period AS p1 ON i1.date=p1.date " + 
-           "INNER JOIN Period AS p2 ON p1.id=p2.id-1 " + 
-           "INNER JOIN Item AS i2 ON i2.date=p2.date AND i1.symbolId = i2.symbolId " + 
-           "WHERE i2.date=?1 AND i2.open/i1.open between 0.8 and 1.9 " + 
-           "GROUP BY p2.id, p2.date")
+    @Query("""
+        SELECT avg(i2.open/i1.open) AS mean FROM Item AS i1
+	       INNER JOIN Period AS p1 ON i1.date=p1.date
+           INNER JOIN Period AS p2 ON p1.id=p2.id-1
+           INNER JOIN Item AS i2 ON i2.date=p2.date AND i1.symbolId = i2.symbolId
+           WHERE i2.date=?1
+            AND i2.open/i1.open between 0.8 and 1.9 
+            AND i1.open*i1.volume>50000
+           GROUP BY p2.id, p2.date
+    """)
     Optional<Double> computeMeanByDate(LocalDate date);
 }
 
