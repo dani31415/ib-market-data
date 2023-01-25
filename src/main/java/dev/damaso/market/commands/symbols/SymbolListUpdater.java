fdd046e9228dc0f87deb22ffcc3e3f47d3762377
dani31415@ <dani31415@gmail.com>
2023-01-25 18:20:29 +0100

    Improved symbol updater.

--- src/main/java/dev/damaso/market/commands/symbols/SymbolListUpdater.java
+++ src/main/java/dev/damaso/market/commands/symbols/SymbolListUpdater.java
@@ -2,9 +2,11 @@ package dev.damaso.market.commands.symbols;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
 import dev.damaso.market.entities.Symbol;
@@ -26,8 +28,92 @@ public class SymbolListUpdater {
     Api api;
 
     public void run() {
+        symbolChangesOverTime();
         addNewSymbols();
         updateConids();
+    }
+
+    public void symbolChangesOverTime() {
+        Iterable<Symbol> iterableSymbol = symbolRepository.findAll();
+        for (Symbol symbol : iterableSymbol) {
+            if (symbol.ib_conid != null && symbol.ib_conid.length()>0) {
+                try {
+                    dev.damaso.market.external.ibgw.ContractInfoResult cir = api.contractInfo(symbol.ib_conid);
+                    if (cir.underlyingConId > 0) {
+                        if (!symbol.disabled) {
+                            symbol.disabled = true;
+                            symbol.updatedAt = LocalDateTime.now(ZoneId.of("UTC"));
+                            System.out.println("Disable symbol " + symbol.id);
+                            symbolRepository.save(symbol);
+                            // Add underlying symbol (with empty data)
+                            Optional<Symbol> existingSymbol = symbolRepository.findByIb_conid("" + cir.underlyingConId);
+                            if (!existingSymbol.isPresent()) {
+                                Symbol newSymbol = new Symbol();
+                                newSymbol.shortName = cir.symbol;
+                                newSymbol.ib_conid = "" + cir.underlyingConId;
+                                newSymbol.createdAt = LocalDateTime.now(ZoneId.of("UTC"));
+                                symbolRepository.save(newSymbol);
+                            }
+                        }
+                    } else {
+                        if (symbol.disabled) {
+                            System.out.println("Why disabled symbol? " + symbol.id);
+                        }
+                    }
+                    if (!symbol.shortName.equals(cir.symbol)) {
+                        // Symbol changed name
+                        System.out.println(symbol.ib_conid + ":" + symbol.shortName + " (old) !=" + cir.symbol + " (new)");
+                        boolean nameUsed = findEnabledName(cir.symbol, iterableSymbol);
+                        // It is safe to rename?
+                        if (symbol.disabled || !nameUsed) {
+                            if (symbol.oldNames != null && symbol.oldNames.length()>0) {
+                                symbol.oldNames += "," + symbol.shortName;
+                            } else {
+                                symbol.oldNames = symbol.shortName;
+                            }
+                            symbol.shortName = cir.symbol;
+                            symbol.updatedAt = LocalDateTime.now(ZoneId.of("UTC"));
+                            System.out.println("Rename symbol");
+                            symbolRepository.save(symbol);
+                        } else {
+                            System.out.println("Not possible to rename");
+                        }
+                    }
+                } catch (IllegalStateException err) {
+                    System.out.println("Error (illegal) for " + symbol.shortName + " " + symbol.id + " '" + symbol.ib_conid + "'");
+                } catch (HttpClientErrorException.BadRequest badRequest) {
+                    if (!symbol.disabled) {
+                        // 1. Find new ib_conid
+                        try {
+                            SearchResult[] results = api.iserverSecdefSearch(symbol.shortName);
+                            String conid = null;
+                            for (SearchResult result : results) {
+                                if (result.description.equals("NASDAQ")) {
+                                    conid = result.conid;
+                                }
+                            }
+                            if (conid != null) {
+                                System.out.println("Recoverable " + symbol.shortName + " " + symbol.id + " '" + symbol.ib_conid + "' with new conid " + conid);
+                                // 2. disable old symbol
+                                symbol.disabled = true;
+                                symbol.updatedAt = LocalDateTime.now(ZoneId.of("UTC"));
+                                symbolRepository.save(symbol);
+                                // 3. enable new symbol with new ib_conid
+                                Symbol newSymbol = new Symbol();
+                                newSymbol.shortName = symbol.shortName;
+                                newSymbol.ib_conid = conid;
+                                newSymbol.createdAt = LocalDateTime.now(ZoneId.of("UTC"));
+                                symbolRepository.save(newSymbol);
+                            } else {
+                                System.out.println("Error (bad) for " + symbol.shortName + " " + symbol.id + " '" + symbol.ib_conid + "'");
+                            }
+                        } catch (Exception err) {
+                            System.out.println("Failed to get symbol " + symbol.shortName + " " + symbol.id + " '" + symbol.ib_conid + "'");
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public void updateConids() {
@@ -104,5 +190,15 @@ public class SymbolListUpdater {
             counter++;
         }
         System.out.println(String.format("%d/%d", old+found, counter));
+    }
+
+
+    private boolean findEnabledName(String name, Iterable<Symbol> symbols) {
+        for (Symbol symbol : symbols) {
+            if (!symbol.disabled && symbol.shortName.equals(name)) {
+                return true;
+            }
+        }
+        return false;   
     }
 }
--- src/main/java/dev/damaso/market/repositories/SymbolRepository.java
+++ src/main/java/dev/damaso/market/repositories/SymbolRepository.java
@@ -1,5 +1,7 @@
 package dev.damaso.market.repositories;
 
+import java.util.Optional;
+
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -14,4 +16,7 @@ public interface SymbolRepository extends CrudRepository<Symbol, Integer> {
 
     @Query(nativeQuery = true, value = "SELECT s.* FROM symbol s WHERE s.ib_conid IS NOT NULL ORDER BY s.id ASC")
     Iterable<Symbol> findAllIB();
+
+    @Query(nativeQuery = true, value = "SELECT s.* FROM symbol s WHERE s.ib_conid = ?1")
+    Optional<Symbol> findByIb_conid(String ib_conid);
 }
