887d67f574621c8507b8d44a0121ddbcb38c7790
dani31415@ <dani31415@gmail.com>
2023-01-26 15:13:18 +0100

    Changed number of items to snapshot.

--- src/main/java/dev/damaso/market/commands/snapshot/Snapshot.java
+++ src/main/java/dev/damaso/market/commands/snapshot/Snapshot.java
@@ -54,7 +54,7 @@ public class Snapshot {
         Iterable<Symbol> symbols = symbolRepository.findAllIB();
         List<Symbol> pendingSymbolList = new Vector<>();
         SnapshotState state = new SnapshotState();
-        int minimumResults = 3000;
+        int minimumResults = 2950;
         int existing = 0;
 
         Iterator<Symbol> iterator = symbols.iterator();
