25191cd6b9e86570a379ccdbb3f7215c8af62281
dani31415@ <dani31415@gmail.com>
2023-01-30 14:52:37 +0100

    Changed minimum resuls to 3200.

--- src/main/java/dev/damaso/market/commands/snapshot/Snapshot.java
+++ src/main/java/dev/damaso/market/commands/snapshot/Snapshot.java
@@ -54,7 +54,7 @@ public class Snapshot {
         Iterable<Symbol> symbols = symbolRepository.findAllIB();
         List<Symbol> pendingSymbolList = new Vector<>();
         SnapshotState state = new SnapshotState();
-        int minimumResults = 2950;
+        int minimumResults = 3200;
         int existing = 0;
 
         Iterator<Symbol> iterator = symbols.iterator();
