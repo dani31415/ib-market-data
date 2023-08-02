95e5c9c80661f95e9b8f86ed0c68096e809d5ae0
dani31415@ <dani31415@gmail.com>
2023-08-02 17:15:52 +0200

    Added log.

--- src/main/java/dev/damaso/market/commands/performance/Performance.java
+++ src/main/java/dev/damaso/market/commands/performance/Performance.java
@@ -3,6 +3,11 @@ package dev.damaso.market.commands.performance;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.sql.DataSource;
 
@@ -31,21 +36,38 @@ public class Performance {
         // Date date = Date.valueOf("2023-02-07");
         // System.out.println(date);
         // pstmt.setDate(2, date);
-        PreparedStatement pstmt = connection.prepareStatement("SELECT open, close, high, low, minute FROM market.minute_item WHERE symbol_id=? ");
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        Map<Long, PreparedStatement> statements = new HashMap<>();
+        // PreparedStatement pstmt = connection.prepareStatement("SELECT open, close, high, low, minute FROM market.minute_item WHERE symbol_id=? ");
         for (int i=1;i<1000;i++) {
-            pstmt.setInt(1, i);
-            ResultSet result = pstmt.executeQuery();
-            int counter = 0;
-            while (result.next()) {
-                counter++;
-                result.getFloat(1);
-                result.getFloat(2);
-                result.getFloat(3);
-                result.getFloat(4);
-                result.getInt(5);
-            }
-            result.close();
-            System.out.println(i + ": " + counter);
+            final int j = i;
+            executor.submit( () -> {
+                try {
+                    PreparedStatement pstmt = statements.get(Thread.currentThread().getId());
+                    if (pstmt == null) {
+                        System.out.println("Created statement for " + Thread.currentThread().getId());
+                        pstmt = connection.prepareStatement("SELECT open, close, high, low, minute FROM market.minute_item WHERE symbol_id=? ");
+                        statements.put(Thread.currentThread().getId(), pstmt);
+                    }
+                    pstmt.setInt(1, j);
+                    ResultSet result = pstmt.executeQuery();
+                    int counter = 0;
+                    while (result.next()) {
+                        counter++;
+                        result.getFloat(1);
+                        result.getFloat(2);
+                        result.getFloat(3);
+                        result.getFloat(4);
+                        result.getInt(5);
+                    }
+                    result.close();
+                    System.out.println(j + ": " + counter);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
         }
+        executor.shutdown();
+        executor.awaitTermination(7200, TimeUnit.SECONDS);
     }
 }
